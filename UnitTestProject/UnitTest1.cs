878c96746808d7c9db88d5be4aec737b8b46ff35
Allan2Fernandes <allan2fernandes@hotmail.com>
2022-11-02 18:58:26 +0100

    Changed button colour

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NNTPProject.Model;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DatabaseHandler DBHandler = new Databasehandler();
+        }
+    }
+}
